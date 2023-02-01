40b1d0b172e9e843b9f85a92dcdeb4d306c01989
aileenok <aileenok02@gmail.com>
2023-02-01 10:04:11 +0900

    yujin gamefeed setting

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Wallet from './Wallet'
 import NotFound from './NotFound'
 import Sample from './Sample'
+import GameFeed from './GameFeed'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Sample />}></Route>
 					<Route path="/wallet/*" element={<Wallet />}></Route>
+					<Route path="/gamefeed/*" element={<GameFeed />}></Route>
 					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
 					<Route path="*" element={<NotFound />}></Route>
 				</Routes>
