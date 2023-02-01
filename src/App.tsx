d82cb450a5d417f261cf24332661c8da5ce17ca8
soenwoo <ynwoo34@gmail.com>
2023-02-01 12:24:11 +0900

    mygames app.tsx 라우터

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NotFound from './NotFound'
 import Sample from './Sample'
 import GameFeed from './GameFeed'
 import Explorer from './Explorer';
+import MyGames from './MyGames';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
 					<Route path="/wallet/*" element={<Wallet />}></Route>
 					<Route path="/gamefeed/*" element={<GameFeed />}></Route>
 					<Route path="/explorer/*" element={<Explorer />}></Route>
+					<Route path="/mygames/*" element={<MyGames />}></Route>
 					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
 					<Route path="*" element={<NotFound />}></Route>
 				</Routes>
