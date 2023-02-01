1844c0221d5c0c1aa07ded1ab37a98fb413579f4
soenwoo <ynwoo34@gmail.com>
2023-02-01 15:01:19 +0900

    explorer, mygames 초안

--- src/MyGames.tsx
+++ src/MyGames.tsx
@@ -8,6 +8,10 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 
 
@@ -15,6 +19,7 @@ import Avatar from '@mui/material/Avatar';
 export default function Explorer() {
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <Container maxWidth="sm">
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -30,32 +35,76 @@ export default function Explorer() {
           <Button color="inherit" variant="outlined">Connect Wallet</Button>
         </Toolbar>
       </AppBar>
-
+      </Container>
       <br></br>
       <br></br>
       <br></br>
       
+      <Container maxWidth="sm">
       <Avatar 
       alt="Remy Sharp" 
       src="/static/images/avatar/1.jpg"
-      sx={{width: 100, height: 100}} />
+      sx={{width: 100, height: 100, alignItem: 'center'}}
+       />
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>User name</Typography>
       <br></br>
       <p>afbb51e46e0f5579ad71ea46c</p>
+      </Container>
       <br></br>
 
       <br></br>
       <br></br>
       <br></br>
 
-      <h3>Recent Played</h3>
-      <Container maxWidth="md">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '50vh' }} />
-      </Container>
+      <Container maxWidth="sm"><h3>Recent Played</h3></Container>
+      
+      
+      <Container maxWidth="sm">
+      <Card sx={{ }}>
+      <CardMedia
+        sx={{ height: 200 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card> 
+    </Container>
       <br></br>
-      <Container maxWidth="md">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '50vh' }} />
-      </Container>
+
+    <Container maxWidth="sm">
+      <Card sx={{ }}>
+      <CardMedia
+        sx={{ height: 200 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card> 
+    </Container>
       <br></br>
 
 
