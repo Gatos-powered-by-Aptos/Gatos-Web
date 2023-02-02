5499dfe49691ecfd8f9cecaa14553c4ee297ef89
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2023-02-03 06:48:57 +0900

    before onchain interaction

--- src/Explorer.tsx
+++ src/Explorer.tsx
@@ -30,7 +30,7 @@ import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Carousel from 'react-material-ui-carousel';
-
+import { Link } from "react-router-dom";
 
 
 import promotion1 from './assets/images/promotion1.jpeg';
@@ -108,6 +108,14 @@ export default function Explorer() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
     React.useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -207,7 +215,6 @@ export default function Explorer() {
   
   
     );
-
   
 
 
@@ -225,8 +232,60 @@ export default function Explorer() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon sx={{color:'#0ACFFE'}}/>
+            <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleClose}>
+        <Link to="/">Home</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/gamefeed">GameFeed</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/mygames">MyGames</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/community">Community</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/onchain">Test</Link>
+        </MenuItem>
+      </Menu>
           </IconButton>
           <div>
             <img src={gatosLogo} alt="logo" width="80vw"/>
--- src/MyGames.tsx
+++ src/MyGames.tsx
@@ -19,6 +19,10 @@ import WorkspacePremiumOutlinedIcon from '@mui/icons-material/WorkspacePremiumOu
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import gatosLogo from './assets/images/GATOS.png';
+import { Link } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design"; 
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
@@ -73,11 +77,19 @@ export default function Explorer() {
           setAccountHasList(false);
         }
       };
+      const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+      const open = Boolean(anchorEl);
+      const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+      };
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
       
       
   return (
     <div>
-    <Box sx={{ flexGrow: 1, backgroundColor:'black' }}>
+  <Box sx={{ flexGrow: 1, backgroundColor:'black'}}>
       <Container maxWidth="xl">
       <AppBar position="static"
         sx={{backgroundColor:'black'}}>
@@ -87,15 +99,67 @@ export default function Explorer() {
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 0 }}
+            sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon sx={{color:'#0ACFFE'}}/>
+            <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleClose}>
+        <Link to="/">Home</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/gamefeed">GameFeed</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/mygames">MyGames</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/community">Community</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Link to="/onchain">Test</Link>
+        </MenuItem>
+      </Menu>
           </IconButton>
           <div>
             <img src={gatosLogo} alt="logo" width="80vw"/>
           </div>
           <Box sx={{width:'500vw'}}></Box>
-          <WalletSelector/>
+          <WalletSelector></WalletSelector>
         </Toolbar>
       </AppBar>
       </Container>
